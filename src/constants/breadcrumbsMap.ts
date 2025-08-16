0cec147efc81b4c70bd7b847c91e6289d0aa01ca
Mudriman <nmudro@mail.ru>
2025-08-16 11:26:42 +0300

    Deploy fresh build

--- src/constants/breadcrumbsMap.ts
+++
@@ -1,8 +0,0 @@
-export const breadcrumbsMap: Record<string, string> = {
-    services: "Услуги",
-    "pvc-membrane": "Монтаж ПВХ-мембраны",
-    "roof-repair": "Ремонт кровли",
-    "flat-roof": "Устройство плоской кровли",
-    "our-works": "Наши работы",
-    "about-us": "О нас",
-};
